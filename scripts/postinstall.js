bda4b04d92e6b1d8d21304689f519af695b5b0be
lif3ng <lif3ng@icloud.com>
2020-12-12 22:05:15 +0800

    fix: init install / ci postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,13 +1,23 @@
 const fs = require("fs");
-const path=require('path')
+const path = require("path");
 console.log("postinstall start");
 
 const cmdArgv = JSON.parse(process.env.npm_config_argv).original;
 
+const cmdStr = cmdArgv.join(" ");
+
+console.log({ cmdArgv });
 // copy file to public
-if (cmdArgv.includes("page-playground")) {
-    console.log('copy html.min.js')
-    fs.copyFileSync(path.join(__dirname,'../node_modules/page-playground/wcdist/html.min.js'),path.join(__dirname,'../docs/.vuepress/public/html.min.js'))
+if (
+  cmdArgv.includes("page-playground") ||
+  cmdStr === "" ||
+  cmdStr === "install"
+) {
+  console.log("copy html.min.js");
+  fs.copyFileSync(
+    path.join(__dirname, "../node_modules/page-playground/wcdist/html.min.js"),
+    path.join(__dirname, "../docs/.vuepress/public/html.min.js")
+  );
 }
 
 console.log("postinstall end");
